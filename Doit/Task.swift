cb9ac0384294c0bd6e97b8c114b4ab0de70ef0cd
Harris Novljakovic <harrisnovljakovic@Harriss-MacBook-Pro.local>
2017-06-11 15:44:51 +0200

    update

---
+++ Doit/Task.swift
@@ -0,0 +1,16 @@
+//
+//  Task.swift
+//  Doit
+//
+//  Created by Harris Novljakovic on 2017-06-11.
+//  Copyright © 2017 Harris Novljakovic. All rights reserved.
+//
+
+import Foundation
+
+
+class Task {
+    var name = ""
+    var important = false
+    
+}
